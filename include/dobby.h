c2df9102c40b660a91d463f79253ca5c4ffb4566
fesily <fesil@foxmail.com>
2023-02-23 13:02:59 +0800

    fix windows error

--- include/dobby.h
+++ include/dobby.h
@@ -108,13 +108,13 @@ typedef struct {
 } DobbyRegisterContext;
 #endif
 
-#define install_hook_name(name, fn_ret_t, fn_args_t...)                                                                \
-  static fn_ret_t fake_##name(fn_args_t);                                                                              \
-  static fn_ret_t (*orig_##name)(fn_args_t);                                                                           \
+#define install_hook_name(name, fn_ret_t, fn_args_t, ...)                                                                \
+  static fn_ret_t fake_##name(fn_args_t, __VA_ARGS__);                                                                              \
+  static fn_ret_t (*orig_##name)(fn_args_t, __VA_ARGS__);                                                                           \
   /* __attribute__((constructor)) */ static void install_hook_##name(void *sym_addr) {                                 \
     DobbyHook(sym_addr, (dobby_dummy_func_t)fake_##name, (dobby_dummy_func_t *)&orig_##name);                          \
     return;                                                                                                            \
-  }                                                                                                                    \
+  }\                                                                                                                    \
   fn_ret_t fake_##name(__VA_ARGS__)
 
 // memory code patch
